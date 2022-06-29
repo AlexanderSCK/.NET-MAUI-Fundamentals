e2ce085201e9efca7c701f519f31be2ec4792968
Alexander Petrov <alexander.ivaylov.petrov@gmail.com>
2022-06-29 14:05:37 +0200

    Add project files.

---
+++ Services/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using League.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace League.Services
+{
+    public interface IProductService
+    {
+        public Task<List<Product>> GetNAsync(int n);
+        public Task<Product> GetBySkuAsync(string sku);
+        public Task CreateAsync(Product product);
+        public Task<Product> UpdateAsync(Product product);
+        public Task DeleteAsync(string sku);
+    }
+}
